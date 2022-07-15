8211b8ccc91925d5ec29fe31b168e4931c9b7674
morena <web.iamsrn@gmail.com>
2022-07-15 21:31:20 +0400

    init

---
+++ src/main/java/com/morena/citTestBack/CitTestBackApplication.java
@@ -0,0 +1,13 @@
+package com.morena.citTestBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitTestBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitTestBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morena/citTestBack/CitTestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morena.citTestBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitTestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
